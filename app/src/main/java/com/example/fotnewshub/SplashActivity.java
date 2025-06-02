5a36c8714ef20652c4ff3e414c801b66fca88f65
Thilina-Prasath <2022to1546@stu.cmb.ac.lk>
2025-06-02 21:11:53 +0530

    Developed splash screen

---
+++ app/src/main/java/com/example/fotnewshub/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.fotnewshub;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
